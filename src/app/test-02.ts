84dbb414f55918fe87feecb70f36d1f30d812822
Marcel <uduma.matrix@gmail.com>
2021-10-14 03:59:18 +0100

    just concluded the second test

--- src/app/test-02.ts
+++ src/app/test-02.ts
@@ -3,23 +3,41 @@
  * * Bind [field] of [textfield] component to its text input
  * * Pass value of [field] from [textfield] component to [title] property of component [ng-app]
  */
-import { Component, NgModule  } from '@angular/core';
+import { Component, EventEmitter, Injectable, NgModule, Output  } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from '@angular/common';
 
+
 @Component({
     selector : 'textfield',
-    template : '<input type="text" value="" />'
+    template : '<input type="text" [(ngModel)]="field" (keyup)="setField()" value=""  />'
 })
 export class TextField {
-    field = "";
+    @Output() field$ = new EventEmitter<string>();
+    field = '';
+
+    setField(){
+        this.field$.emit(this.field);
+    }
 }
 
 @Component({
     selector : 'child-component',
-    template : `<h2>Title:<h2><br/><textfield></textfield>`
+    template : `<h2>Title:<h2><br/>
+                <textfield (field$)='getField($event)'></textfield>`
 })
 export class ChildComponent {
+    @Output() _field$ = new EventEmitter<string>();
+    
+    getField(f: string){
+        this.titleBridge(f)
+        console.log('ff')
+    }
+
+    titleBridge(t: string){
+        this._field$.emit(t)
+    }
 
 }
 
@@ -27,18 +45,23 @@ export class ChildComponent {
 @Component({
     selector : 'ng-app',
     template : `<div>
-                    <child-component></child-component> <br/>
+                    <child-component (_field$)="setTitle($event)"></child-component> <br/>
                     Title is {{title}}
                 </div>`
 })
 export class Test02Component {
 
-    title:string = "";
+    title:string = "z";
+
+    setTitle(t: string){
+        this.title = t;
+    }
 }
 
 @NgModule({
     imports : [
         CommonModule,
+        FormsModule,
         RouterModule.forChild([
             {
                 path : "",
@@ -46,6 +69,6 @@ export class Test02Component {
             }
         ])
     ],
-    declarations : [Test02Component,ChildComponent,TextField]
+    declarations : [Test02Component,ChildComponent,TextField],
 })
 export class Test02Module {};
